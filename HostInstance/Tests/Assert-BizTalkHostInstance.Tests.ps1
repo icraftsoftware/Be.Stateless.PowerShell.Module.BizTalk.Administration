6f3ca7573659782351a67286519ae8a0b00ece4d
François Chabot <fchabot@skynet.be>
2020-06-04 14:26:38 +0200

    Fix unit tests' hard coded machine name dependency

--- HostInstance/Tests/Assert-BizTalkHostInstance.Tests.ps1
+++ HostInstance/Tests/Assert-BizTalkHostInstance.Tests.ps1
@@ -27,7 +27,7 @@ Describe 'Assert-BizTalkHostInstance' {
 
         Context 'When the BizTalk Server Host Instance does not exist' {
             It 'Throws.' {
-                { Assert-BizTalkHostInstance -Name Inexistent_Host } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Inexistent_Host'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Inexistent_Host } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Inexistent_Host' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
         }
 
@@ -42,28 +42,28 @@ Describe 'Assert-BizTalkHostInstance' {
                 { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled:$false } | Should  -Not -Throw
             }
             It 'Throws when the host instance should be disabled.' {
-                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''BizTalkServerApplication'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'BizTalkServerApplication' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is started.' {
                 { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStarted } | Should  -Not -Throw
             }
             It 'Throws when the host instance should not be started.' {
-                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStarted:$false } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''BizTalkServerApplication'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStarted:$false } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'BizTalkServerApplication' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is not disabled and is started.' {
                 { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled:$false -IsStarted } | Should  -Not -Throw
             }
             It 'Throws when the host instance is started but should be disabled.' {
-                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled -IsStarted } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''BizTalkServerApplication'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled -IsStarted } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'BizTalkServerApplication' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Throws when the host instance is not disabled but should not be started.' {
-                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled:$false -IsStarted:$false } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''BizTalkServerApplication'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsDisabled:$false -IsStarted:$false } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'BizTalkServerApplication' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is not stopped.' {
                 { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStopped:$false } | Should  -Not -Throw
             }
             It 'Throws when the host instance should be stopped.' {
-                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStopped } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''BizTalkServerApplication'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name BizTalkServerApplication -IsStopped } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'BizTalkServerApplication' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
         }
 
@@ -72,28 +72,28 @@ Describe 'Assert-BizTalkHostInstance' {
                 { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled } | Should  -Not -Throw
             }
             It 'Throws when the host instance should not be disabled.' {
-                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled:$false } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Test_Host_1'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled:$false } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Test_Host_1' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is not started.' {
                 { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStarted:$false } | Should  -Not -Throw
             }
             It 'Throws when the host instance should be started.' {
-                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStarted } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Test_Host_1'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStarted } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Test_Host_1' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is stopped.' {
                 { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStopped } | Should  -Not -Throw
             }
             It 'Throws when the host instance should not be stopped.' {
-                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStopped:$false } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Test_Host_1'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsStopped:$false } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Test_Host_1' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Does not throw when the host instance is disabled and is stopped.' {
                 { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled -IsStopped } | Should  -Not -Throw
             }
             It 'Throws when the host instance is stopped but should be disabled.' {
-                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled:$false -IsStopped } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Test_Host_1'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled:$false -IsStopped } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Test_Host_1' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
             It 'Throws when the host instance is disabled but should not be stopped.' {
-                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled -IsStopped:$false } | Should -Throw -ExpectedMessage 'Microsoft BizTalk Server ''Test_Host_1'' Host Instance on ''Artichaut'' server does not exist or is not in the expected state.'
+                { Assert-BizTalkHostInstance -Name Test_Host_1 -IsDisabled -IsStopped:$false } | Should -Throw -ExpectedMessage "Microsoft BizTalk Server 'Test_Host_1' Host Instance on '$($Env:COMPUTERNAME)' server does not exist or is not in the expected state."
             }
         }
 
